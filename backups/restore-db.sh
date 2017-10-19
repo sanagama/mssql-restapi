fceed50d15f81c8edf0df377ba8cef60aad582d8
Sanjay Nagamangalam <sanagama2@gmail.com>
2017-10-19 16:29:02 -0700

    Added WWI restore scripts

--- backups/restore-db.sh
+++ backups/restore-db.sh
@@ -6,3 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # run sqlcmd inside the Docker container to restore Adventureworks
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -i /backups/restore-adventureworks.sql
 
+# run sqlcmd inside the Docker container to restore WideWorldImporters
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -i /backups/restore-wwi.sql
