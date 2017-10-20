3a062a8db7d854fb7776ab825e60fef72f15c871
Sanjay Nagamangalam <sanagama2@gmail.com>
2017-10-19 21:38:31 -0700

    changed to mssql-restapi

--- scripts/1-docker-mssql.sh
+++ scripts/1-docker-mssql.sh
@@ -4,7 +4,7 @@ set -x
 docker pull microsoft/mssql-server-linux:2017-GA
 
 docker run  --name 'sqldocker' --cap-add SYS_PTRACE \
-            -v  ~/mssql-webapi/backups:/backups \
+            -v  ~/mssql-restapi/backups:/backups \
             -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=Yukon900' \
             -e 'MSSQL_PID=Developer' \
             -p 1433:1433 \
